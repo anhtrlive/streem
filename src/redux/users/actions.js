920cfdd99fdf5656c0a27a2cc79553327b11eb76
anhtrlive <artyukhovmaxim@fastmail.com>
2020-12-07 16:43:05 +0200

    delete inf scroll

--- src/redux/users/actions.js
+++ src/redux/users/actions.js
@@ -2,8 +2,8 @@ import mockapi from '../../apis/mockapi';
 
 export const FETCH_USERS = 'FETCH_USERS';
 
-export const fetchUsers = (pageNumber = 1) => async (dispatch) => {
-  const response = await mockapi.get(`/users?page=${pageNumber}&limit=10`);
+export const fetchUsers = () => async (dispatch) => {
+  const response = await mockapi.get(`/users`);
 
   return dispatch({
     type: FETCH_USERS,
