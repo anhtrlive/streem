10693540bf77419e0a08b20d3fcc704dbf9ea7b0
anhtrlive <artyukhovmaxim@fastmail.com>
2020-12-07 17:35:35 +0200

    install and add BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
+import { Switch, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
+
 import Homepage from '../pages/homepage/homepage';
 
 export default function App() {
   return (
     <Container style={{ margin: 20 }}>
-      <Homepage />
+      <Switch>
+        <Route exact path="/search"></Route>
+        <Route path="/:userId"></Route>
+        <Route path="/users/:userId/posts/:postId"></Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './components/App';
 import store from './redux/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.querySelector('#root')
 );
